6702a6b6a932d35ceaf5ee1c12b1a7900cf7500a
joelordasjr <joelordasjr@gmail.com>
2014-08-01 18:45:06 +0800

    added require login to user

--- thesis_manager.py
+++ thesis_manager.py
@@ -118,6 +118,20 @@ class Guestbook2(webapp2.RequestHandler):
         query_params = {'guestbook_name': guestbook_name}
         self.redirect('/module-1/2?' + urllib.urlencode(query_params))
 
+
+class SigninHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
+
+        self.response.out.write("<html><body>%s</body></html>" % greeting)
+
+
 class MemberOnePage(webapp2.RequestHandler):
     
     def get(self):
@@ -422,5 +436,5 @@ application = webapp2.WSGIApplication([
     ('/thesis/edit/(.*)', ThesisEditHandler),
     ('/student/edit/(.*)', StudentEditHandler),
     ('/adviser/edit/(.*)', AdviserEditHandler),
-
+    ('/signin',SigninHandler),     
 ], debug=True)
